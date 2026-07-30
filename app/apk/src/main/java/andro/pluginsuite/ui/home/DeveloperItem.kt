1e9d946d5d51263ef22756d7bd0cdc73804a2bd3
unknown <getime10000@gmail.com>
2026-07-30 10:42:33 +0800

    Clean up developers list: keep topjohnwu + Getime-Xiatian, remove twitter/paypal/patreon

--- app/apk/src/main/java/andro/pluginsuite/ui/home/DeveloperItem.kt
+++ app/apk/src/main/java/andro/pluginsuite/ui/home/DeveloperItem.kt
@@ -13,20 +13,8 @@ private interface JohnImpl : Dev {
     override val name get() = "topjohnwu"
 }
 
-private interface VvbImpl : Dev {
-    override val name get() = "vvb2060"
-}
-
-private interface YUImpl : Dev {
-    override val name get() = "yujincheng08"
-}
-
-private interface RikkaImpl : Dev {
-    override val name get() = "RikkaW"
-}
-
-private interface CanyieImpl : Dev {
-    override val name get() = "canyie"
+private interface GetimeImpl : Dev {
+    override val name get() = "Getime-Xiatian"
 }
 
 sealed class DeveloperItem : Dev {
@@ -37,41 +25,14 @@ sealed class DeveloperItem : Dev {
     object John : DeveloperItem(), JohnImpl {
         override val items =
             listOf(
-                object : IconLink.Twitter(), JohnImpl {},
                 IconLink.Github.Project
             )
     }
 
-    object Vvb : DeveloperItem(), VvbImpl {
+    object Getime : DeveloperItem(), GetimeImpl {
         override val items =
             listOf<IconLink>(
-                object : IconLink.Twitter(), VvbImpl {},
-                object : IconLink.Github.User(), VvbImpl {}
-            )
-    }
-
-    object YU : DeveloperItem(), YUImpl {
-        override val items =
-            listOf<IconLink>(
-                object : IconLink.Twitter() { override val name = "shanasaimoe" },
-                object : IconLink.Github.User(), YUImpl {},
-                object : IconLink.Sponsor(), YUImpl {}
-            )
-    }
-
-    object Rikka : DeveloperItem(), RikkaImpl {
-        override val items =
-            listOf<IconLink>(
-                object : IconLink.Twitter() { override val name = "rikkawww" },
-                object : IconLink.Github.User(), RikkaImpl {}
-            )
-    }
-
-    object Canyie : DeveloperItem(), CanyieImpl {
-        override val items =
-            listOf<IconLink>(
-                object : IconLink.Twitter() { override val name = "canyie2977" },
-                object : IconLink.Github.User(), CanyieImpl {}
+                object : IconLink.Github.User(), GetimeImpl {}
             )
     }
 }
@@ -84,28 +45,6 @@ sealed class IconLink : RvItem() {
 
     override val layoutRes get() = R.layout.item_icon_link
 
-    abstract class PayPal : IconLink(), Dev {
-        override val icon get() = CoreR.drawable.ic_paypal
-        override val title get() = CoreR.string.paypal
-        override val link get() = "https://paypal.me/$name"
-
-        object Project : PayPal() {
-            override val name: String get() = "magiskdonate"
-        }
-    }
-
-    object Patreon : IconLink() {
-        override val icon get() = CoreR.drawable.ic_patreon
-        override val title get() = CoreR.string.patreon
-        override val link get() = Const.Url.PATREON_URL
-    }
-
-    abstract class Twitter : IconLink(), Dev {
-        override val icon get() = CoreR.drawable.ic_twitter
-        override val title get() = CoreR.string.twitter
-        override val link get() = "https://twitter.com/$name"
-    }
-
     abstract class Github : IconLink() {
         override val icon get() = CoreR.drawable.ic_github
         override val title get() = CoreR.string.github
@@ -117,11 +56,5 @@ sealed class IconLink : RvItem() {
         object Project : Github() {
             override val link get() = Const.Url.SOURCE_CODE_URL
         }
-    }
-
-    abstract class Sponsor : IconLink(), Dev {
-        override val icon get() = CoreR.drawable.ic_favorite
-        override val title get() = CoreR.string.github
-        override val link get() = "https://github.com/sponsors/$name"
     }
 }
